5f8a63dd226afdf87e205c528ebf71df43aaaf01
Maks Vlasov <vla5924@gmail.com>
2017-07-24 12:22:43 +0300

    Update ruhtml.js

--- ruhtml.js
+++ ruhtml.js
@@ -1,7 +1,7 @@
 function ruhtml(ru) 
 {  
   var text = $('#'+ru).val();  
-  text = text.split('\n').join('<br/>').split('[l]').join('<div align=\'left\'>').split('[/l]').join('</div>').split('[r]').join('<div align=\'right\'>').split('[/r]').join('</div>').split('[c]').join('<div align=\'center\'>').split('[/c]').join('</div>').split('[img]').join('<img src=\'').split('[/img]').join('\'/>').split('[url').join('<a href').split('url]').join('a>').split('[size').join('<font size').split('size]').join('font>').split('[color').join('<font color').split('color]').join('font>').split('<').join('<').split('[').join('<').split(']').join('>').split('[/').join('</');  
+  text = text.split('\n').join('<br/>').split('<блок').join('<div').split('</блок>').join('</div>').split('выравн=').join('align=').split('<стр').join('<span').split('</стр>').join('</span>').split('[/c]').join('</div>').split('<и ').join('<img').split(' ист=').join(' src=').split('[url').join('<a href').split('url]').join('a>').split('[size').join('<font size').split('size]').join('font>').split('[color').join('<font color').split('color]').join('font>').split('<').join('<').split('[').join('<').split(']').join('>').split('[/').join('</');  
   if(text == '') text = 'Сначала введите ruHTML-код в поле выше!';  
   $("#result").html(text);  
 }
